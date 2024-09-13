a411c9d17464cc2c806159b53f8ca924b96ed125
suntechgithub <sugumars@suntechnologies.com>
2024-09-13 21:13:01 +0530

    renamed the file

--- tests/Qbank_demo.spec.js
+++ tests/Qbank_demo.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const testdata = require('../TestData/json_testData.json');
+const testdata = require('../fixtures/json_testData.json');
 
 test.describe('QBANK', async () => {
 
